b64f025e75a3c140a10374adfbac19ae51aa1daf
dev_tk <dev_tk@int.pl>
2022-10-07 20:43:41 +0200

    add Content-Security-Policy 2

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000;
 /** Decode Form URL Encoded data */
 app.use(function (req, res, next) {
   res.setHeader(
-    'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
   );
-  
   next();
 });
 app.use(cors());
